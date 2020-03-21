08328d73cdd0394633278f7eeafc7290f2637aae
RexMud <rex@limatech.co.ke>
2020-03-21 09:23:31 +0300

    added a default end time and refactored code for readability

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -14,22 +14,30 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(641, 459)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.qr_code_icon = QtWidgets.QLabel(self.centralwidget)
         self.qr_code_icon.setGeometry(QtCore.QRect(30, 30, 231, 211))
         self.qr_code_icon.setObjectName("qr_code_icon")
+
         self.dateEdit_in = QtWidgets.QDateEdit(self.centralwidget)
         self.dateEdit_in.setGeometry(QtCore.QRect(430, 120, 181, 26))
         self.dateEdit_in.setObjectName("dateEdit_in")
         self.dateEdit_in.setDateTime(QtCore.QDateTime.currentDateTime())
+
         self.start_time_in = QtWidgets.QTimeEdit(self.centralwidget)
         self.start_time_in.setGeometry(QtCore.QRect(430, 170, 181, 26))
         self.start_time_in.setObjectName("start_time_in")
         self.start_time_in.setTime(QtCore.QTime.currentTime())
+
         self.end_time_in = QtWidgets.QTimeEdit(self.centralwidget)
         self.end_time_in.setGeometry(QtCore.QRect(430, 210, 181, 26))
         self.end_time_in.setObjectName("end_time_in")
+        # TODO: change the +3 below to a user defined value for flexibility
+        self.end_time_in.setTime(QtCore.QTime(QtCore.QTime.currentTime().hour()+3, QtCore.QTime.currentTime().minute()))
+
         self.course_code_in = QtWidgets.QLineEdit(self.centralwidget)
         self.course_code_in.setGeometry(QtCore.QRect(360, 80, 251, 25))
         self.course_code_in.setText("")
@@ -40,54 +48,77 @@ class Ui_MainWindow(object):
         self.course_name_in.setText("")
         self.course_name_in.setPlaceholderText("Enter course name")
         self.course_name_in.setObjectName("course_name_in")
+
         self.time_start_label = QtWidgets.QLabel(self.centralwidget)
         self.time_start_label.setGeometry(QtCore.QRect(360, 170, 71, 21))
         self.time_start_label.setObjectName("time_start_label")
+
         self.end_time_label = QtWidgets.QLabel(self.centralwidget)
         self.end_time_label.setGeometry(QtCore.QRect(360, 210, 61, 21))
         self.end_time_label.setObjectName("end_time_label")
+
         self.date_label = QtWidgets.QLabel(self.centralwidget)
         self.date_label.setGeometry(QtCore.QRect(360, 120, 71, 21))
         self.date_label.setObjectName("date_label")
+
         self.save_btn = QtWidgets.QPushButton(self.centralwidget)
         self.save_btn.setGeometry(QtCore.QRect(70, 280, 89, 25))
         self.save_btn.setObjectName("pushButton")
+
         self.export_btn = QtWidgets.QPushButton(self.centralwidget)
         self.export_btn.setGeometry(QtCore.QRect(70, 330, 89, 25))
         self.export_btn.setObjectName("pushButton_2")
+
         self.create_new_btn = QtWidgets.QPushButton(self.centralwidget)
         self.create_new_btn.setGeometry(QtCore.QRect(430, 320, 89, 25))
         self.create_new_btn.setObjectName("pushButton_3")
+
         self.create_btn = QtWidgets.QPushButton(self.centralwidget)
         self.create_btn.setGeometry(QtCore.QRect(430, 270, 89, 25))
         self.create_btn.clicked.connect(self.on_create_clicked)
         self.create_btn.setObjectName("pushButton_4")
+
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 641, 22))
         self.menubar.setObjectName("menubar")
+
         self.menuQRDesk = QtWidgets.QMenu(self.menubar)
         self.menuQRDesk.setObjectName("menuQRDesk")
+
         self.menuReports = QtWidgets.QMenu(self.menubar)
         self.menuReports.setObjectName("menuReports")
+
         self.menuSettings = QtWidgets.QMenu(self.menubar)
         self.menuSettings.setObjectName("menuSettings")
+
         MainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+
         MainWindow.setStatusBar(self.statusbar)
+
         self.actionCreate_QR_Code = QtWidgets.QAction(MainWindow)
         self.actionCreate_QR_Code.setObjectName("actionCreate_QR_Code")
+
         self.actionView_Reports = QtWidgets.QAction(MainWindow)
         self.actionView_Reports.setObjectName("actionView_Reports")
+
         self.actionUser_Settings = QtWidgets.QAction(MainWindow)
         self.actionUser_Settings.setObjectName("actionUser_Settings")
+
         self.actionCheck_for_updates = QtWidgets.QAction(MainWindow)
         self.actionCheck_for_updates.setObjectName("actionCheck_for_updates")
+
         self.menuQRDesk.addAction(self.actionCreate_QR_Code)
+
         self.menuReports.addAction(self.actionView_Reports)
+
         self.menuSettings.addAction(self.actionUser_Settings)
         self.menuSettings.addAction(self.actionCheck_for_updates)
+
         self.menubar.addAction(self.menuQRDesk.menuAction())
         self.menubar.addAction(self.menuReports.menuAction())
         self.menubar.addAction(self.menuSettings.menuAction())
